c4dfbd390b5e489157fecc3e4cb7f95d4d37e9dc
Julio <julio06cesar11@gmail.com>
2024-09-18 01:04:24 -0300

    adcioando_db

--- frontend/src/pages/Atualizar.jsx
+++ frontend/src/pages/Atualizar.jsx
@@ -1,4 +1,22 @@
-function  Atualizar(){
-    return (<div> Atualizar</div>)
+function  Atualizar({
+        cor,
+        tipo,   
+        fornecedor,
+        qtd_mts,
+        setCor,
+        setTipo,
+        setFornecedor,
+        setQtdMts,
+        handleSubmit,
+        dataPost}){
+    return (<div>
+        <form onSubmit={handleSubmit} method="POST">
+            <label>Cor<input type="text" value={cor} onChange={(e)=> setCor(e.target.value)}/></label>
+            <label>Tipo<input type="text" value={tipo} onChange={(e)=> setTipo(e.target.value)}/></label>
+            <label>Fornecedor<input type="text" value={fornecedor} onChange={(e)=> setFornecedor(e.target.value)}/></label>
+            <label>qtd_mts<input type="number" value={qtd_mts} onChange={(e)=> setQtdMts(e.target.value)}/></label>
+            <button type="submit">Add</button>
+        </form>
+    </div>)
 }
 export default Atualizar;
--- frontend/src/pages/Visualizar.jsx
+++ frontend/src/pages/Visualizar.jsx
@@ -1,6 +1,19 @@
 
 
 function Visualizar(){
-    return (<div>Visualizar</div>)
+    return (<div>
+            <h1>Controle DE Estoque</h1>
+            <nav className="space-x-9 flex flex-row justify-center">
+                <button className="w-36 h-11 bg-blue-950 shadow-md hover:focus:bg-yellow-300 rounded-xl">A</button>
+                <button className="w-36 h-11 bg-green-500 shadow-md hover:focus:bg-yellow-300 rounded-xl">A</button>
+                <button className="w-36 h-11 bg-orange-500 shadow-md hover:focus:bg-yellow-300 rounded-xl">A</button>
+                <button className="w-36 h-11 bg-red-500 shadow-md hover:focus:bg-yellow-300 rounded-xl">A</button>
+                <button className="w-36 h-11 bg-gray-500 shadow-md  hover:focus:bg-yellow-300 rounded-xl">A</button>
+            </nav>
+            <div className="flex flex-row justify-center">
+                <input className="mt-8  w-96 h-16 bg-gray-200 text-red-600" type="text" name="pesquisa" id="pesquisa" /><button className="w-24 h-16 mt-8 ml-3 space-x-2 bg-green-400 text-white">Buscar</button>
+            </div>
+            
+        </div>)
 }
 export default Visualizar;
